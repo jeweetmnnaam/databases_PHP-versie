48dc9eac23dd5a24794e3f36ae0dd21e976dee2c
jeweetmnnaam <153071@csgaugustinus.nl>
2023-04-19 10:51:05 +0000

    vavaviiet

---
+++ databases/sneeuwmuis.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS `sneeuwmuis`;
+CREATE DATABASE `sneeuwmuis`;
+USE `sneeuwmuis`;
+CREATE TABLE `dieren` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `product` text NOT NULL,
+    `prijs` int NOT NULL
+);
+
+INSERT INTO `dieren` (`id`,`product`,`prijs`) VALUES
+(1, `Cavia`, 40),
+(2, `Goudvis`, 50),
+(3, `Kat`, 100);
